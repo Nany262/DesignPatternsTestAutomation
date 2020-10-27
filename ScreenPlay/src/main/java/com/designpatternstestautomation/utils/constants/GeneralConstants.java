8b77bc81a315c3e9e95e7d036e093d7569010936
Juan Pablo Becerra <jpbecerra1019@gmail.com>
2020-10-27 08:27:11 -0500

    Creación constans, modificación builgradle, configuración stepdefinitions.

---
+++ ScreenPlay/src/main/java/com/designpatternstestautomation/utils/constants/GeneralConstants.java
@@ -0,0 +1,7 @@
+package com.designpatternstestautomation.utils.constants;
+
+public class GeneralConstants {
+    public static final String AUTOMATION_PAGE = "http://automationpractice.com/index.php";
+    public static final String CLIENT_USER = "Client User";
+    public static final String CHROME = "chrome";
+}
