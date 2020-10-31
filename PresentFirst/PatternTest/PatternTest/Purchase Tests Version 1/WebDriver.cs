da89b339702a809c56a3bf80c928993b6c1e1cd5
juliandg16 <juliandg16@gmail.com>
2020-10-31 18:25:24 -0500

    implementation of decorator Desing patter

---
+++ PresentFirst/PatternTest/PatternTest/Purchase Tests Version 1/WebDriver.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace PatternTest.Purchase_Tests_Version_1
+{
+    public class WebDriver : Driver
+    {
+        private IWebDriver _webDriver;
+        private WebDriverWait _webDriverWait;
+
+        public override void Start(Browser browser)
+        {
+            switch (browser)
+            {
+                case Browser.Chrome:
+                    _webDriver = new ChromeDriver(Environment.CurrentDirectory);
+                    break;
+
+                case Browser.Firefox:
+                    _webDriver = new FirefoxDriver(Environment.CurrentDirectory);
+                    break;
+
+                case Browser.Edge:
+                    _webDriver = new EdgeDriver(Environment.CurrentDirectory);
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(browser),browser,null);
+            }
+            _webDriverWait = new WebDriverWait(_webDriver,TimeSpan.FromSeconds(30));
+            _webDriverWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            _webDriverWait.IgnoreExceptionTypes(typeof(WebDriverException));
+        }
+
+        public override void Quit()
+        {
+            _webDriver.Quit();
+        }
+
+        public override void GoToUrl(string url)
+        {
+            _webDriver.Navigate().GoToUrl(url);
+        }
+
+        public override Element FindElement(By locator)
+        {
+            IWebElement nativeWebElement=_webDriverWait.Until(drv=>drv.FindElement(locator));
+            Element element = new WebElement(_webDriver, nativeWebElement, locator);
+
+            //if we use log decorator
+            LogElement logElement = new LogElement(element);
+
+            return logElement;
+        }
+
+        public override List<Element> FindElements(By locator)
+        {
+            ReadOnlyCollection<IWebElement> nativeWebElements = _webDriverWait.Until(drv => drv.FindElements(locator));
+            List<Element> elements = new List<Element>();
+            foreach (var nativeWebElement in nativeWebElements)
+            {
+                Element element = new WebElement(_webDriver, nativeWebElement, locator);
+                elements.Add(element);
+            }
+            return elements;
+        }
+    }
+}
---
+++ PresentFirst/PatternTest/PatternTest/Purchase Tests Version 1/WebElement.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace PatternTest.Purchase_Tests_Version_1
+{
+    public class WebElement : Element
+    {
+        private IWebDriver _webDriver;
+        private IWebElement _webElement;
+        private By _by;
+
+        private WebElement(IWebDriver webDriver, IWebElement webElement, By by)
+        {
+            _webDriver = webDriver;
+            _webElement = webElement;
+            _by = by;
+        }
+
+        public override By By
+        {
+            get
+            {
+                return By;
+            }
+        }
+
+        public override string Text
+        {
+            get
+            {
+                return _webElement?.Text;
+            }
+        }
+
+        public override bool? Enabled {
+            get
+            {
+                return _webElement?.Enabled;
+            }
+        }
+
+        public override bool? Displayed
+        {
+            get
+            {
+                return _webElement?.Displayed;
+            }
+        }
+
+        public override void Click()
+        {
+            WaitToBeClickable(By);
+            _webElement.Click();
+        }
+
+        public override string GetAttribute(string attributeName)
+        {
+            return _webElement?.GetAttribute(attributeName);
+        }
+
+        public override void TypeText(string text)
+        {
+            _webElement?.Clear();
+            _webElement?.SendKeys(text);
+        }
+
+        private void WaitToBeClickable(By by, int timeOutInSeconds = 30)
+        {
+            if(timeOutInSeconds > 0)
+            {
+                var wait = new WebDriverWait(_webDriver,TimeSpan.FromSeconds(timeOutInSeconds));
+                wait.IgnoreExceptionTypes(typeof(WebDriverException));
+                wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+                wait.IgnoreExceptionTypes(typeof(NoSuchElementException),typeof(StaleElementReferenceException));
+                //wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(by));
+            }
+        }
+    }
+}
