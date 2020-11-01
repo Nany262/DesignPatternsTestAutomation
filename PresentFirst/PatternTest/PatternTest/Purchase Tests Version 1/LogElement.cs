4de6e07378f02bd34305518a33fdf5c7687fc2d7
juliandg16 <juliandg16@gmail.com>
2020-10-31 19:10:47 -0500

    elements

--- PresentFirst/PatternTest/PatternTest/Purchase Tests Version 1/LogElement.cs
+++ PresentFirst/PatternTest/PatternTest/Purchase Tests Version 1/LogElement.cs
@@ -3,11 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace PatternTest.Purchase_Tests_Version_1
 {
     public class LogElement : ElementDecorator
     {
+        public LogElement(Element element)
+            : base(element)
+        {
 
+        }
+        public override By By
+        {
+            get
+            {
+                return By;
+            }
+        }
+        public override string Text
+        {
+            get
+            {
+                return element?.Text;
+            }
+        }
+        public override bool? Displayed
+        {
+            get
+            {
+                return element?.Displayed;
+            }
+        }
+        public override bool? Enabled 
+        {
+            get
+            {
+                return element?.Enabled;
+            }
+        }
+        public override string GetAttribute(string attributeName)
+        {
+            return element?.GetAttribute(attributeName);
+        }
+        public override void TypeText(string text)
+        {
+             element?.TypeText(text);
+        }
     }
 }
